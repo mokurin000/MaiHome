dc85f1900f1fa8bc91a50cd10447ac9151fefe7b
莯凛 <mokurin000@gmail.com>
2025-07-18 16:26:07 +0800

    Create convert.py

---
+++ utils/convert.py
@@ -0,0 +1,20 @@
+import subprocess
+from multiprocessing import Pool
+from os import listdir, path
+
+
+def process(dir: str):
+    if not path.isdir(dir):
+        return
+    subprocess.run(
+        ["ffmpeg", "-i", f"{dir}/1.wav", "-qscale:a", "2", "-b:a", "128k", f"{dir}.mp3"]
+    )
+
+
+def main():
+    with Pool() as pool:
+        pool.map(process, listdir("."))
+
+
+if __name__ == "__main__":
+    main()
